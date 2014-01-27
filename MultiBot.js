707b19af7000e66fc24f2aa414888448261856e5
xSidewinderx <xSidewinderx@users.noreply.github.com>
2014-01-27 05:35:46 -0600

    Update MultiBot.js

--- MultiBot.js
+++ MultiBot.js
@@ -107,10 +107,10 @@ var BOT = {
             API.on(API.WAIT_LIST_UPDATE, BOT.INTERNAL.waitlist);
             BOT.registerJqueryEvent($(window).bind('beforeunload', BOT.INTERNAL.close));
 // WAIT TO ENABLE ALL PLUGINS & MODULES
-            BOT.sendAlert("I'm now running!");
+            BOT.sendAlert("");
         },
             close: function(e) {
-            BOT.sendAlert("Reloading!");
+            BOT.sendAlert("");
             console.log('Removing TempBans');
             var users = BOT.INTERNAL.allUsers;
             var bans = BOT.INTERNAL.tempBans;
